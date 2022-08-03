46225a963be5adfdf2c01914e64673252f11282f
Eiwelton <eliwelton.g.p@gmail.com>
2022-08-03 14:00:59 -0300

    implementado nav bar

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <Navbar :logo="logo_src" :alt="app_name" />
+  <router-view />
+  <Footer />
+</template>
+
+<script>
+import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
+
+export default {
+  components: {
+    Navbar,
+    Footer,
+  },
+  data() {
+    return {
+      logo_src: "/img/logo.png",
+      app_name: "Faça seu Hamburguer",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
